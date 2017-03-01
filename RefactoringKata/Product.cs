b73a517778f47fcef10bf37bcae1ff82560bc7eb
Stanly Lau <stanly@odd-e.com>
2017-03-01 20:31:23 +0800

    Added set;

--- RefactoringKata/Product.cs
+++ RefactoringKata/Product.cs
@@ -4,11 +4,11 @@
     {
         public static int SIZE_NOT_APPLICABLE = -1;
 
-        public string Code { get; }
-        public int Color { get; }
-        public int Size { get; }
-        public double Price { get; }
-        public string Currency { get; }
+        public string Code { get; set; }
+        public int Color { get; set; }
+        public int Size { get; set; }
+        public double Price { get; set; }
+        public string Currency { get; set; }
 
         public Product(string code, int color, int size, double price, string currency)
         {
